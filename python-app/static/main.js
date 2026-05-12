b9f1cb44556ca7c274004a7b02e51a80265c1d5f
sporteiro <seblash@gmail.com>
2026-05-12 18:17:19 +0100

    new style

--- python-app/static/main.js
+++ python-app/static/main.js
@@ -6,17 +6,14 @@ class Main {
 
     init() {
         this.get_countries();
-        let self = this;
-        $(document).ready(function () {
-            $("#select_country").change(function () {
-                let now_code = $(this).val();
-                if (now_code !== self.selected_country) {
-                    self.selected_country = now_code;
-                    self.get_news_from_country(now_code);
-                }
-            });
-        });
         $("#app_info").hide();
+
+        // Close dropdown when clicking outside
+        $(document).on('click', function(e) {
+            if (!$(e.target).closest('.custom-dropdown').length) {
+                $('.custom-dropdown').removeClass('open');
+            }
+        });
     }
 
     get_countries() {
@@ -26,7 +23,7 @@ class Main {
             dataType: 'json',
             success: function (data) {
                 main.country_list = data;
-                main.build_country_select(data);
+                main.build_custom_dropdown(data);
             },
             error: function (xhr, status, error) {
                 console.log(xhr.status, error);
@@ -34,17 +31,55 @@ class Main {
         });
     }
 
-    build_country_select(countries) {
-        $("#select_country").empty();
-        $.each(countries, function (i, country) {
-            let option = '<option value="' + country.code + '">' + country.name + '</option>';
-            $("#select_country").append(option);
+    build_custom_dropdown(countries) {
+        var self = this;
+        var $selectContainer = $('#select_country').parent(); // El div.country-selector
+        var $select = $('#select_country');
+        $select.hide(); // Ocultamos el select nativo
+
+        // Creamos el dropdown personalizado
+        var $dropdown = $('<div class="custom-dropdown"></div>');
+        var $trigger = $('<div class="custom-dropdown-trigger"></div>');
+        var $list = $('<div class="custom-dropdown-list"></div>');
+
+        // Llenamos con los datos de países
+        $.each(countries, function(i, country) {
+            var flagPath = 'country-flags-main/svg/' + country.code.toLowerCase() + '.svg';
+            var $item = $('<div class="custom-dropdown-item" data-value="' + country.code + '">' +
+                '<img src="' + flagPath + '" alt="' + country.name + '">' +
+                '<span>' + country.name + '</span>' +
+                '</div>');
+
+            $item.on('click', function() {
+                var value = $(this).data('value');
+                if (value !== self.selected_country) {
+                    self.selected_country = value;
+                    self.get_news_from_country(value);
+                    // Actualizar el trigger
+                    $trigger.html($(this).html());
+                    $dropdown.removeClass('open');
+                }
+            });
+
+            $list.append($item);
+
+            // Seleccionar el primero por defecto
+            if (i === 0) {
+                $trigger.html('<img src="' + flagPath + '" alt="' + country.name + '"><span>' + country.name + '</span>');
+                self.selected_country = country.code;
+                self.get_news_from_country(country.code);
+            }
         });
-        if (countries.length > 0) {
-            $("#select_country").val(countries[0].code);
-            main.selected_country = countries[0].code;
-            main.get_news_from_country(countries[0].code);
-        }
+
+        // Evento toggle del dropdown
+        $trigger.on('click', function(e) {
+            e.stopPropagation();
+            $(this).parent().toggleClass('open');
+        });
+
+        $dropdown.append($trigger);
+        $dropdown.append($list);
+        $selectContainer.append($dropdown);
     }
 
     get_news_from_country(country_code) {
